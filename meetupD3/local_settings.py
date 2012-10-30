27bdea13b6e2c6d673b2e753d1b34c8bd87f9818
Caleb Hyde <caleb.hyde@gmail.com>
2012-10-30 10:25:15 +0000

    Insert Memcached middleware

--- meetupD3/local_settings.py
+++ meetupD3/local_settings.py
@@ -19,3 +19,13 @@ TEMPLATE_DIRS = (
     '/home/caleb/dev/meetupD3/templates',
 )
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
+CACHE_MIDDLEWARE_ALIAS='default'
+CACHE_MIDDLEWARE_SECONDS=60*3
+CACHE_MIDDLEWARE_KEY_PREFIX=''
