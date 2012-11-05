3fe6e3fa0b46ff2042e0439e1f6b3a4a024a8261
Caleb Hyde <caleb.hyde@gmail.com>
2012-11-05 13:19:14 +0000

    Add framework and color styles. Move path settings to local_settings.

--- meetupD3/local_settings.py
+++ meetupD3/local_settings.py
@@ -1,5 +1,14 @@
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+TEMPLATE_DEBUG = True
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'gp*#q6en1308)2yw_wt*e9lds2pnxju&amp;!4)^^s9eie9s#9eg)c'
+
+INTERNAL_IPS = {
+    '127.0.0.1',
+    '10.0.2.15',
+    '10.0.2.2',
+}
 
 DATABASES = {
     'default': {
@@ -17,6 +26,33 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     '/home/caleb/dev/meetupD3/templates',
+)
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = ''
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = ''
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = '/home/caleb/dev/meetupD3/static'
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/styles/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    '/home/caleb/dev/meetupD3/app1/styles',
 )
 
 #from settings_cache import *
--- meetupD3/settings.py
+++ meetupD3/settings.py
@@ -29,32 +29,6 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
@@ -62,9 +36,6 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'gp*#q6en1308)2yw_wt*e9lds2pnxju&amp;!4)^^s9eie9s#9eg)c'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
