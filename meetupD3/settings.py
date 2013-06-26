9d9eec73baf74c265d39093f10f5e319084deb2e
Caleb Hyde <caleb.hyde@gmail.com>
2013-06-26 03:02:59 +0000

    Revert relative paths

--- meetupD3/settings.py
+++ meetupD3/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+#    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'meetupD3.urls'
@@ -70,7 +70,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'debug_toolbar',
+#    'debug_toolbar',
     'app1',
 )
 
